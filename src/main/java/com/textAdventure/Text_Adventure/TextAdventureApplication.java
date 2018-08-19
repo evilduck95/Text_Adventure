7ce57ccbb86fcc230dd984f60b8dee4492734252
evilduck95 <keiranbrown95@gmail.com>
2018-08-19 17:22:51 +0100

    Added files to repo

---
+++ src/main/java/com/textAdventure/Text_Adventure/TextAdventureApplication.java
@@ -0,0 +1,12 @@
+package com.textAdventure.Text_Adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAdventureApplication.class, args);
+	}
+}
