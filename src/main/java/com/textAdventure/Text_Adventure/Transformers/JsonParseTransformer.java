2d0913850874234bb051f2ad73b26f8d244658e5
evilduck95 <keiranbrown95@gmail.com>
2018-09-27 19:43:40 +0100

    Created transformer to decrypt payload (it was a byte[]) and read json tree!

---
+++ src/main/java/com/textAdventure/Text_Adventure/Transformers/JsonParseTransformer.java
@@ -0,0 +1,30 @@
+package com.textAdventure.Text_Adventure.Transformers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.transformer.Transformer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+import java.io.IOException;
+
+@Configuration
+public class JsonParseTransformer implements Transformer {
+
+    @Override
+    public Message<?> transform(Message<?> message) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode payloadJson = mapper.readTree(new String((byte[]) message.getPayload()));
+            System.out.println(payloadJson.get("str"));
+            return new GenericMessage<>(payloadJson);
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return null;    // TODO: Will we ever reach here???
+    }
+
+}
--- src/main/java/com/textAdventure/Text_Adventure/Web/JsonController.java
+++ src/main/java/com/textAdventure/Text_Adventure/Web/JsonController.java
@@ -1,9 +1,10 @@
 package com.textAdventure.Text_Adventure.Web;
 
+import com.textAdventure.Text_Adventure.Transformers.JsonParseTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.http.dsl.Http;
 import org.springframework.stereotype.Controller;
 
@@ -11,14 +12,21 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class JsonController {
 
+    @Autowired
+    private JsonParseTransformer jsonParseTransformer;
+
     @Bean
     public IntegrationFlow jsonInputRouter() {
-        return IntegrationFlows.from(Http.inboundGateway("/json")).log().channel("jsonInputChannel").get();
+        return IntegrationFlows.from(Http.inboundGateway("/json"))
+                .channel("jsonInputChannel")
+                .get();
     }
 
     @Bean
     public IntegrationFlow testFlow() {
-        return IntegrationFlows.from("jsonInputChannel").log(LoggingHandler.Level.INFO, m -> "Header: " + m.getHeaders()).get();
+        return IntegrationFlows.from("jsonInputChannel")
+                .transform(jsonParseTransformer)
+                .get();
     }
 
 
