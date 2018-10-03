e3f5668b2751dae8c1175ac361ef53a54b60599d
evilduck95 <keiranbrown95@gmail.com>
2018-10-03 20:30:38 +0100

    Got a filter working!

--- src/main/java/com/textAdventure/Text_Adventure/Web/JsonController.java
+++ src/main/java/com/textAdventure/Text_Adventure/Web/JsonController.java
@@ -1,11 +1,14 @@
 package com.textAdventure.Text_Adventure.Web;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.textAdventure.Text_Adventure.Transformers.JsonParseTransformer;
+import com.textAdventure.Text_Adventure.Utils.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.http.dsl.Http;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Controller;
 
@@ -40,14 +43,18 @@ public class JsonController {
                 .get();
     }
 
-    //TODO: Make my own damned filter!
     @Bean
     public IntegrationFlow jsonRoutingFlow() {
         return IntegrationFlows.from(jsonRoutingChannel)
-                .filter(f ->)
-                .channel("nowhere")
+                .filter(Message.class, m -> getActionType(m).equals("testItem"))
+                .handle(m -> Logger.info("Filter Passed!"))
                 .get();
     }
 
+    private String getActionType(Message<?> message) {
+        JsonNode messageJson = (JsonNode) message.getPayload();
+        Logger.info(messageJson.get("actionType").textValue());
+        return messageJson.get("actionType").textValue();
+    }
 
 }
